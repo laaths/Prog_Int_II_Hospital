7195e1e36145a053e3c0733c1a058289854545f4
Jonatan Goulart <jonatan784@gmail.com>
2021-11-22 21:59:37 -0300

    Arrumado o problema de Autenticação de Token e preparação para OAuth

--- rotas/login_rota.js
+++ rotas/login_rota.js
@@ -7,4 +7,11 @@ const usuarioController = require('../controller/usuario_controller');
 
 rota.post('/', usuarioController.validarUsuario);
 
+
+const { requiresAuth } = require('express-openid-connect');
+
+rota.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
 module.exports = rota;
