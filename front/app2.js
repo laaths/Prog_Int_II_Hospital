0b4fd5b3b69a927a3152e8b4d94584145d05cd3c
Jonatan Goulart <jonatan784@gmail.com>
2021-12-01 23:40:14 -0300

    Coloca o index para o root e adiciona função inicial de tabela de usuarios
    Verificar se o caminho do script esta correto.

--- front/app2.js
+++ front/app2.js
@@ -18,14 +18,29 @@ function buscarPessoas() {
 function buscarProdutos() {
     let xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        const listaProdutos = JSON.parse(this.responseText);
-        let lista = `<ul>`;
-        for (let i = 0; i < listaProdutos.length; i++) {
-            lista += `<li>${listaProdutos[i].nome}</li>`;
+        const listaUsuarios = JSON.parse(this.responseText);
+        let lista = `<table>`;
+        lista += `<tr>`
+        for (let i = 0; i < listaUsuarios.length; i++) {
+            lista += `<td>${listaUsuarios[i].nome}</td>`;
         }
-        lista = '</ul>';
+        lista += `</tr>`
+
+        lista += `<tr>`
+        for (let i = 0; i < listaUsuarios.length; i++) {
+            lista += `<td>${listaUsuarios[i].email}</td>`;
+        }
+        lista += `</tr>`
+
+        lista += `<tr>`
+        for (let i = 0; i < listaUsuarios.length; i++) {
+            lista += `<td>${listaUsuarios[i].username}</td>`;
+        }
+        lista += `</tr>`
+
+        lista += '</table>';
         main.innerHTML = lista;
     }
-    xhttp.open("GET", "http://localhost:3000/produtos", true);
+    xhttp.open("GET", "https://progintiihosp.herokuapp.com/usuarios/listar", true);
     xhttp.send();
 }
