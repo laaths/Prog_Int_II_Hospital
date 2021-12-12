de1011100e73c97ab0772b875736e5d9d05111af
Jonatan Goulart <jonatan784@gmail.com>
2021-12-12 13:44:09 -0300

    Update app2.js

--- front/app2.js
+++ front/app2.js
@@ -18,14 +18,37 @@ function buscarPessoas() {
 function buscarProdutos() {
     let xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
+
         const listaProdutos = JSON.parse(this.responseText);
         let lista = `<ul>`;
         for (let i = 0; i < listaProdutos.length; i++) {
             lista += `<li>${listaProdutos[i].nome}</li>`;
+
+            const listaUsuarios = JSON.parse(this.responseText);
+            let lista = `<table>`;
+            lista += `<tr>`
+            for (let i = 0; i < listaUsuarios.length; i++) {
+                lista += `<tr>
+                <td>${listaUsuarios[i].nome}</td>; <
+            /tr>`
+            }
+            lista += `</tr>`
+
+            lista += `<tr>`
+            for (let i = 0; i < listaUsuarios.length; i++) {
+                lista += `<td>${listaUsuarios[i].email}</td>`;
+            }
+            lista += `</tr>`
+
+            lista += `<tr>`
+            for (let i = 0; i < listaUsuarios.length; i++) {
+                lista += `<td>${listaUsuarios[i].username}</td>`;
+
+            }
+            lista = '</ul>';
+            main.innerHTML = lista;
         }
-        lista = '</ul>';
-        main.innerHTML = lista;
+        xhttp.open("GET", "http://localhost:3000/produtos", true);
+        xhttp.send();
     }
-    xhttp.open("GET", "http://localhost:3000/produtos", true);
-    xhttp.send();
 }
