0218a0c7d8cd4cce11c74a94f8f67c36549e229c
Leonardo Duarte <53016531+laaths@users.noreply.github.com>
2021-12-12 00:22:36 -0300

    Listar pessoas ou Fila. Iniciando função busca Fila com nome.

--- front/app2.js
+++ front/app2.js
@@ -1,37 +1,101 @@
 let body = document.querySelector("body");
 let main = document.querySelector("main");
-/*
+let tabelaElemento = document.querySelector("#tabela");
+let formElemento = document.querySelector("#formulario")
+let buscaFila = document.querySelector("#buscafila")
+
 body.onload = function() {
-<<<<<<< Updated upstream
     console.log("Inicializando o body");
-    // window.alert("Hello World");
-    buscarPessoas();
-    setInterval(buscarPessoas, 5000);
-=======
-    console.log("Loading o body");
-    buscarProdutos();
-    // setInterval(buscarProdutos, 5000);
->>>>>>> Stashed changes
+    buscarPessoasFila();
+    setInterval(buscarPessoasFila, 5000);
+    FomrmularioPessoa()
 }
-*/
+
 let cont = 1;
 
-function buscarPessoas() {
-    // console.log(cont++);
-    //main.innerHTML = cont++;
+function buscarPessoasFila() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        const listapessoas = JSON.parse(this.responseText);
+        let lista = ['<table>','<tr>','<th>Pessoa</th>','<th>Classificação</th>','<th>Numero Fila</th>', '</tr>'];
+        for (let i = 0; i < listapessoas.length; i++) {
+            lista += '<tr>'
+            lista += `<td>${listapessoas[i].pessoaid}</td>`;
+            lista += `<td>${listapessoas[i].classificacao}</td>`;
+            lista += `<td>${listapessoas[i].nfila}</td>`;
+            lista += '</tr>'
+        }
+        lista += '</table>';
+        buscaFila.innerHTML = lista;
+    }
+        xhttp.open("GET", "http://localhost:3000/fila/listar", true);
+        xhttp.send();
 }
 
-function buscarProdutos() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        const listaProdutos = JSON.parse(this.responseText);
-        let lista = `<ul>`;
-        for (let i = 0; i < listaProdutos.length; i++) {
-            lista += `<li>${listaProdutos[i].nome}</li>`;
+
+
+
+
+function FomrmularioPessoa() {
+    const formulario = `<form id="formFila">
+        <label for='nomeInput'>Nome:</label>
+        <input id='nomeInput'> </br>
+        <label type='number' for='idadeInput'>Idade:</label>
+        <input id='idadeInput'> </br>
+        <input type="submit" value="Salvar">
+    </form>`;
+
+    formElemento.innerHTML = formulario;
+
+    const formProdutos = document.querySelector("#formProdutos");
+    formProdutos.onsubmit = function(event) {
+        event.preventDefault();
+        const nomeInput = document.querySelector("#nomeInput");
+        const idadeInput = document.querySelector("#idadeInput");
+        if(nomeInput.value && idadeInput.value) {
+            let pessoas = new Object();
+            pessoas.nome = nomeInput.value;
+            pessoas.preco = +idadeInput.value;
+            //Chamada AJAX para inserir produto
+            inserirProduto(pessoas);
+            nomeInput.value="";
+            idadeInput.value="";
         }
-        lista = '</ul>';
-        main.innerHTML = lista;
+        else {
+            alert("Campos Nome e Preco obrigatórios");
+        }
     }
-    xhttp.open("GET", "http://localhost:3000/usuarios", true);
+}
+
+function inserirProduto(pessoas) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+        const pessoas = JSON.parse(this.responseText);
+        alert(`Produto ${pessoas.nome} cadastrado com sucesso!`);
+        buscarPessoasFila();
+    }
+    xhttp.open("POST", "localhost:3000/pessoas/inserir", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(pessoas));
+
+}/* //em construção
+function chamaFila(){
+    let xhttp = new XMLHttpRequest();
+    let xhtt2 = new XMLHttpRequest();
+    xhttp.onload = function () {
+        const listapessoas = JSON.parse(this.responseText);
+        for (let i = 0; i < listapessoas.length; i++) {
+        buscaFila.innerHTML = lista;
+    }
+    xhttp2.onload = function () {
+        const listapessoas = JSON.parse(this.responseText);
+        for (let i = 0; i < listapessoas.length; i++) {
+            buscaFila.innerHTML = lista;
+        }
+    xhttp.open("GET", "http://localhost:3000/fila/listar", true);
+    xhttp.open("GET", "http://localhost:3000/pessoas/listar", true);
+
     xhttp.send();
 }
+}
+*/
