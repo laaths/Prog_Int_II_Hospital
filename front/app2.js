037337a07aebc560792a9f93f017fe3c8160d9fb
Jonatan Goulart <jonatan784@gmail.com>
2021-12-12 23:37:07 -0300

    Arruma Atendimento, e estiliza o home.html

--- front/app2.js
+++ front/app2.js
@@ -4,19 +4,22 @@ let tabelaElemento = document.querySelector("#tabela");
 let formElemento = document.querySelector("#formulario");
 let buscaFila = document.querySelector("#buscafila");
 let buscaPessoas = document.querySelector("#buscaPessoas");
+let buscaAtendimento = document.querySelector("#buscaAtendimento");
 
 body.onload = function() {
     console.log("Inicializando o body");
-    buscarPessoasFila();
+    buscarFila();
     buscarPessoas();
+    buscarAtendimento();
     setInterval(buscarPessoas, 10000);
-    setInterval(buscarPessoasFila, 10000);
-    formularioPessoa()
+    setInterval(buscarFila, 10000);
+    setInterval(buscarAtendimento, 10000);
+    formularioPessoa();
 }
 
 let cont = 1;
 
-function buscarPessoasFila() {
+function buscarFila() {
     let xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         const listapessoas = JSON.parse(this.responseText);
@@ -53,13 +56,31 @@ function buscarPessoas() {
     xhttp.send();
 }
 
+function buscarAtendimento() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        const listaAtendimento = JSON.parse(this.responseText);
+        let lista = ['<table>' + '<tr>' + '<th>Nome do Paciente</th>' + '<th>Médico Responsável</th>' + '</tr>'];
+        for (let i = 0; i < listaAtendimento.length; i++) {
+            lista += '<tr>'
+            lista += `<td>${listaAtendimento[i].pessoanome}</td>`;
+            lista += `<td>${listaAtendimento[i].mediconome}</td>`;
+            lista += '</tr>'
+        }
+        lista += '</table>';
+        buscaAtendimento.innerHTML = lista;
+    }
+    xhttp.open("GET", "http://localhost:3000/atendimento/listar", true);
+    xhttp.send();
+}
+
 function formularioPessoa() {
     const formulario = `<form id="formPessoas">
         <label for='nomeInput'>Nome:</label>
         <input id='nomeInput'> </br>
         <label type='number' for='idadeInput'>Idade:</label>
         <input id='idadeInput'> </br>
-        <input type="submit" value="Salvar">
+        <button type="submit" value="Enviar">Enviar</button>
     </form>`;
 
     formElemento.innerHTML = formulario;
